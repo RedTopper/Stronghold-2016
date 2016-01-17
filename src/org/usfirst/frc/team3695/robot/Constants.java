964fed32cb3419428daa319ebd7530477883f41d
AJ Walter <aj.walter@aol.com>
2016-01-16 21:29:43 -0600

    Whoops

--- src/org/usfirst/frc/team3695/robot/Constants.java
+++ src/org/usfirst/frc/team3695/robot/Constants.java
@@ -1,5 +1,9 @@
 package org.usfirst.frc.team3695.robot;
 
 public class Constants {
-
+	private Constants() throws InstantiationException{
+		throw new InstantiationException();
+	}
+	
+	public static final int FRONT_LEFT_MOTOR_PORT = 1;
 }
