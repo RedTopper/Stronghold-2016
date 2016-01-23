6fc3ecc6c7094688d143e226701d73c3bfe24011
kbowen99 <kurtisbowen99@gmail.com>
2016-01-23 10:44:20 -0600

    Tutorial Cleanup

--- src/org/usfirst/frc/team3695/robot/OI.java
+++ src/org/usfirst/frc/team3695/robot/OI.java
@@ -21,7 +21,7 @@ public class OI {
 		//SmartDash
 		SmartDashboard.putData("Move 1 Stephen (slow)", new CommandDistance(0.25,1));
 		SmartDashboard.putData("Move 1 Stephen (fast)", new CommandDistance(1,1));
-		SmartDashboard.putData("Move 1 Stephen (6 wheel)", new CommandSecondaryDistance(1,1));
+		//SmartDashboard.putData("Move 1 Stephen (6 wheel)", new CommandSecondaryDistance(1,1));
 		
 		//6 Wheel Drive Button ('Obstacle Button')
 		Button doObstacleMagically = new JoystickButton(driveStick, Constants.ENABLE_6WHEEL_DRIVE);
