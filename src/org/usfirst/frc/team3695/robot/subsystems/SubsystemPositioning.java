1269a9cb7e4be6ce3b8077b393140633c636af53
wh1ter0se <ipad3000@yahoo.com>
2016-02-03 10:14:52 -0600

    Format Setup
    Added command import
    Added package formatting

--- src/org/usfirst/frc/team3695/robot/subsystems/SubsystemPositioning.java
+++ src/org/usfirst/frc/team3695/robot/subsystems/SubsystemPositioning.java
@@ -1,1 +1,11 @@
+//Is this proper format?
+package org.usfirst.frc.team3695.robot.subsystems;
+
+//Load the ultrasonic rangefinder command
+import org.usfirst.frc.team3695.robot.commands.CommandUltrasonicRange;
+
+//Load the FRC ultrasonic class
 import edu.wpi.first.wpilibj.Ultrasonic
+
+//TODO set up positioning system however programming team decides on it
+//TODO I mean we really don't fully understand how we will use this but we wanted to just set it up first
