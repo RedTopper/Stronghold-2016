ea8feeb8fc1dff4d08b603e914777666ebada0a8
Max <mpg1019@aol.com>
2016-02-21 13:57:27 -0800

    ¯\_(ツ)_/¯ Working on the arm solenoid stuff.

---
+++ src/org/usfirst/frc/team3695/robot/commands/CommandLaunchBall.java
@@ -0,0 +1,5 @@
+package org.usfirst.frc.team3695.robot.commands;
+
+public class CommandLaunchBall {
+
+}
---
+++ src/org/usfirst/frc/team3695/robot/subsystems/SubsystemArm.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team3695.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class SubsystemArm extends Subsystem {
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
