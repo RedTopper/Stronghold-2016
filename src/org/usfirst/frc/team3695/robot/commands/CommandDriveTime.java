daaff75348c4d6cef6d342d694812c19ab7c7a11
AJ Walter <aj.walter@aol.com>
2016-03-01 18:45:55 -0600

    CommandDriveTime.java added to the robot

---
+++ src/org/usfirst/frc/team3695/robot/commands/CommandDriveTime.java
@@ -0,0 +1,39 @@
+// Max G, 2016 (<3)
+// CommandDriveTime.java
+// Makes the robot drive forward for a specified amount of time.
+
+package org.usfirst.frc.team3695.robot.commands;
+
+import org.usfirst.frc.team3695.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class CommandDriveTime extends Command {
+	private long startTime = 0;
+	private float driveTime = 0;
+	
+	public CommandDriveTime(float timeToDrive) {
+		driveTime = timeToDrive;
+	}
+	
+	protected void initialize() {
+		startTime = System.currentTimeMillis();
+	}
+
+	protected void execute() {
+		Robot.driveSubsystem.drive(0, -1);
+	}
+
+	protected boolean isFinished() {
+		long now = System.currentTimeMillis();
+		return (now - startTime) > driveTime;
+	}
+
+	protected void end() {
+		Robot.driveSubsystem.drive(0, 0);
+	}
+
+	protected void interrupted() {
+		end();
+	}
+}
