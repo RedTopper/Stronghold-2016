e723cbcc4af72236f53f4d570bae3c8f21fa37dc
AJ Walter <aj.walter@aol.com>
2016-03-20 21:53:26 -0500

    In theory, Autonomous works!

--- src/org/usfirst/frc/team3695/robot/Autonomous.java
+++ src/org/usfirst/frc/team3695/robot/Autonomous.java
@@ -1,10 +1,15 @@
 package org.usfirst.frc.team3695.robot;
 
 import org.usfirst.frc.team3695.robot.commands.CommandDriveTime;
+import org.usfirst.frc.team3695.robot.commands.CommandDriveWithCam;
+import org.usfirst.frc.team3695.robot.commands.CommandRotateWithCam;
+import org.usfirst.frc.team3695.robot.commands.pneumatics.CommandMoveArm;
 import org.usfirst.frc.team3695.robot.commands.pneumatics.CommandMoveBucket;
 import org.usfirst.frc.team3695.robot.enumeration.objective.Defense;
+import org.usfirst.frc.team3695.robot.enumeration.objective.MoveArm;
 import org.usfirst.frc.team3695.robot.enumeration.objective.MoveBucket;
 import org.usfirst.frc.team3695.robot.enumeration.objective.RotateWithCam;
+import org.usfirst.frc.team3695.robot.util.CommandDoNothing;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
@@ -41,9 +46,14 @@ public class Autonomous extends CommandGroup {
 		switch(objectiveDirection) {
 		case NOTHING:
 			break;
-		case ROTATE_LEFT_OVERALL:
-			break;
 		case ROTATE_RIGHT_OVERALL:
+		case ROTATE_LEFT_OVERALL:
+			addSequential(new CommandMoveBucket(MoveBucket.MOVE_UP));
+			addSequential(new CommandRotateWithCam(objectiveDirection));
+			addSequential(new CommandDriveWithCam());
+			addSequential(new CommandMoveBucket(MoveBucket.MOVE_DOWN));
+			addSequential(new CommandDoNothing(1000));
+			addSequential(new CommandMoveArm(MoveArm.FIRE));
 			break;
 		}
 	}
